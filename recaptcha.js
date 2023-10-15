4aca8ee71fd289f7926d0a867f71c13e9803f8fb
isaiah-stock <isaiah.stocK@gmail.com>
2023-10-15 10:35:20 -0700

    changing functionality of email reveal

--- recaptcha.js
+++ recaptcha.js
@@ -1,13 +1,14 @@
 function onSubmit(token) {
-    revealEmail();
-}
-
-function revealEmail() {
-    var emailLink = document.createElement("a");
-    var user = "isaiah.stock@gmail.com";
-    var domain = "isaiahstock.com";
-    
-    emailLink.href = "mailto:" + user + "@" + domain;
-    emailLink.textContent = "Email me";
-    document.body.appendChild(emailLink);
-}
+    // Obtain the button and container elements
+    var button = document.querySelector(".g-recaptcha");
+    var container = button.parentElement;
+  
+    // Create a new paragraph element to display the email
+    var emailParagraph = document.createElement("p");
+    emailParagraph.textContent = "isaiah.stock@gmail.com";
+  
+    // Append the email paragraph to the container and remove the button
+    container.appendChild(emailParagraph);
+    container.removeChild(button);
+  }
+  
